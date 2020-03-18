dd71a00808952a5bb578f98c6fa7f2a183f6e2db
Denys Kovalenko <denys.kovalenko@taxify.eu>
2020-03-18 19:40:55 +0200

    sink wiki data to Redis

--- src/main/java/wikiedits/WikipediaAnalysis.java
+++ src/main/java/wikiedits/WikipediaAnalysis.java
@@ -1,16 +1,45 @@
 package wikiedits;
 
+
 import org.apache.flink.api.common.functions.FoldFunction;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.redis.RedisSink;
+import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
 import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditEvent;
 import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditsSource;
 
 public class WikipediaAnalysis {
+
+    public static class RedisWikiStatsMapper implements RedisMapper<Tuple2<String, Long>> {
+        private String redisKey;
+        public RedisWikiStatsMapper(String baseKey) {
+            this.redisKey=baseKey;
+        }
+
+        @Override
+        public RedisCommandDescription getCommandDescription() {
+            return new RedisCommandDescription(RedisCommand.HSET, this.redisKey);
+        }
+
+        @Override
+        public String getKeyFromData(Tuple2<String, Long> data) {
+            return data.f0;
+        }
+
+        @Override
+        public String getValueFromData(Tuple2<String, Long> data) {
+            return data.f1.toString();
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment see = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -34,7 +63,11 @@ public class WikipediaAnalysis {
                         return acc;
                     }
                 });
-        result.print();
+
+        result.addSink(new PrintSinkFunction());
+
+        FlinkJedisPoolConfig conf = new FlinkJedisPoolConfig.Builder().setHost("127.0.0.1").build();
+        result.addSink(new RedisSink<>(conf, new RedisWikiStatsMapper("wiki_stats")));
 
         see.execute();
     }
